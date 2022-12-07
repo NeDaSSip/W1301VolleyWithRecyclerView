0598cc2c52c02cba5b7bbacf387876c65fe0914d
Kim SeungHwan <olleh2761@naver.com>
2022-12-07 15:03:49 +0900

    13yas

---
+++ app/src/main/java/kr/ac/kumoh/s20180219/w1301volleywithrecyclerview/MainActivity.kt
@@ -0,0 +1,58 @@
+package kr.ac.kumoh.s20180219.w1301volleywithrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kr.ac.kumoh.s20180219.w1301volleywithrecyclerview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var model: SongViewModel
+    private val songAdapter = SongAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        model = ViewModelProvider(this)[SongViewModel::class.java]
+
+        binding.list.apply {
+            layoutManager = LinearLayoutManager(applicationContext)
+            setHasFixedSize(true)
+            itemAnimator = DefaultItemAnimator()
+            adapter = songAdapter
+        }
+
+        model.list.observe(this) {
+            songAdapter.notifyItemRangeInserted(0, model.list.value?.size ?: 0)
+        }
+
+        model.requestSong()
+    }
+
+    inner class SongAdapter: RecyclerView.Adapter<SongAdapter.ViewHolder>() {
+        inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+            val txTitle: TextView = itemView.findViewById(R.id.text1)
+            val txSinger: TextView = itemView.findViewById(R.id.text2)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = layoutInflater.inflate(R.layout.item_song, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.txTitle.text = model.list.value?.get(position)?.title
+            holder.txSinger.text = model.list.value?.get(position)?.singer
+        }
+
+        override fun getItemCount() = model.list.value?.size ?: 0
+    }
+}
